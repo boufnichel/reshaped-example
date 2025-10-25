7e43de0ec14188958815e8adad03ea50df9b6f1d
Boufnichel mohamed <boufnichel.mohamed@gmail.com>
2025-10-25 23:36:44 +0100

    Add Page component with butAdd page component with Reshaped View and Buttonton and view

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import { View, Button } from 'reshaped';
+
+export default function Page() {
+  return (
+    <View padding={2}>
+      <Button href="https://reshaped.so" color="primary">Click me</Button>
+    </View>
+  );
+}
